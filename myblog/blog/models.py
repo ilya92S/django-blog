c449ed0b6cbf44ffbcceb0a5495b635bae268404
ilya <saitovilya26@gmail.com>
2024-07-11 01:26:04 +0300

    make style for blog and add image

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     description = models.TextField('Текст записи')
     author = models.CharField('Имя автора', max_length=100)
     date = models.DateField('Двта публикации')
+    image = models.ImageField('Изображение', upload_to='image/%Y')
 
     class Meta:
         verbose_name = 'Запись'
