64a67b08a528c5668a3420c7afe51515896a7c6a
ilya <saitovilya26@gmail.com>
2024-07-10 23:46:35 +0300

    initial commit

---
+++ myblog/blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Post(models.Model):
+    """Данные о посте в блоге"""
+    title = models.CharField('Заголовок записи', max_length=100)
+    description = models.TextField('Текст записи')
+    author = models.CharField('Имя автора', max_length=100)
+    date = models.DateField('Двта публикации')
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
+
+    def __str__(self):
+        return f'{self.title}, {self.author}'
