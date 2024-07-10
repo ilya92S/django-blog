7ef718f4f53a3ae594a7e7d236ee51970c1ad37c
ilya <saitovilya26@gmail.com>
2024-07-11 00:27:32 +0300

    add project

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import Post
 
 
-# @admin.register(Post)
-# class PostAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'author')
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
